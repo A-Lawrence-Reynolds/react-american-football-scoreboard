073ee7131118dda9b3b9366e25dbd64c0cbaebfe
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-09-10 12:02:52 -0700

    Added more buttons and working on making it more user friendly

--- src/App.js
+++ src/App.js
@@ -1,83 +1,92 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {
-  useState
-} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+
+function formatLeadingZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionScore, setLionScore] = useState(0);
   const [tigerScore, setTigerScore] = useState(0);
+  const [penalties, setPenalties] = useState(0);
+  const [half, setHalf] = useState(0);
+  const [ballOn, setBallOn] = useState(0);
 
+  const durationInSeconds = Math.round(Math.random() * (40 * 60));
+  const seconds = durationInSeconds % 60;
+  const minutes = (durationInSeconds - seconds) / 60;
 
-  return ( <
-    div className = "container" >
-    <
-    section className = "scoreboard" >
-    <
-    div className = "topRow" >
-    <
-    div className = "home" >
-    <
-    h2 className = "home__name" > Lions < /h2>
-
-    {
-      /* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */ }
-
-    <
-    div className = "home__score" > {
-      lionScore
-    } < /div> <
-    /div> <
-    div className = "timer" > 40: 00 < /div> <
-    div className = "away" >
-    <
-    h2 className = "away__name" > Tigers < /h2> <
-    div className = "away__score" > {
-      tigerScore
-    } < /div> <
-    /div> <
-    /div> <
-    BottomRow / >
-    <
-    /section> <
-    section className = "buttons" >
-    <
-    div className = "homeButtons" > {
-      /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */ } <
-    button onClick = {
-      () => setLionScore(lionScore + 4)
-    }
-    className = "homeButtons__touchdown" > Home Tries < /button>
-
-    <
-    button onClick = {
-      () => setLionScore(lionScore + 2)
-    }
-    className = "homeButtons__fieldGoal" > Home Goal Kick < /button> <
-    button onClick = {
-      () => setLionScore(0)
-    }
-    className = "homeButtons__fieldGoal" > Reset Home < /button> <
-    /div> <
-    div className = "awayButtons" >
-    <
-    button onClick = {
-      () => setTigerScore(tigerScore + 4)
-    }
-    className = "awayButtons__touchdown" > Away Tries < /button> <
-    button onClick = {
-      () => setTigerScore(tigerScore + 2)
-    }
-    className = "awayButtons__fieldGoal" > Away Goal Kick < /button> <
-    button onClick = {
-      () => setTigerScore(0)
-    }
-    className = "awayButtons__fieldGoal" > Reset Away < /button> <
-    /div> <
-    /section> <
-    /div>
+  const time = formatLeadingZero(minutes) + ":" + formatLeadingZero(seconds);
+  return (
+    <div className="container">
+      <section className="scoreboard">
+        <div className="topRow">
+          <div className="home">
+            <h2 className="home__name"> Lions </h2>
+            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+            <div className="home__score"> {lionScore} </div>
+          </div>
+          <div className="timer">{time}</div>
+          <div className="away">
+            <h2 className="away__name"> Tigers </h2>
+            <div className="away__score"> {tigerScore} </div>
+          </div>
+        </div>
+        <BottomRow penalties={1} toGo={7} ballOn={21} half={4} />
+      </section>
+      <section className="buttons">
+        <div className="homeButtons">
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          <button
+            onClick={() => setLionScore(lionScore + 4)}
+            className="homeButtons__touchdown"
+          >
+            Home Tries
+          </button>
+          <button
+            onClick={() => setLionScore(lionScore + 2)}
+            className="homeButtons__fieldGoal"
+          >
+            Home Goal Kick
+          </button>
+          <button
+            onClick={() => setLionScore(0)}
+            className="homeButtons__fieldGoal"
+          >
+            Reset Home
+          </button>
+        </div>
+        <div className="awayButtons">
+          <button
+            onClick={() => setTigerScore(tigerScore + 4)}
+            className="awayButtons__touchdown"
+          >
+            Away Tries
+          </button>
+          <button
+            onClick={() => setTigerScore(tigerScore + 2)}
+            className="awayButtons__fieldGoal"
+          >
+            Away Goal Kick
+          </button>
+          <button
+            onClick={() => setPenalties(penalties + 1)}
+            className="awayButtons__fieldGoal"
+          >
+            Penalties
+          </button>
+          <button
+            onClick={() => setTigerScore(0)}
+            className="awayButtons__fieldGoal"
+          >
+            Reset Away
+          </button>
+        </div>
+      </section>
+    </div>
   );
 }
 
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,40 +1,22 @@
-import React, {
-  useState
-} from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-// const[]
-const BottomRow = () => {
-  // const [pen, setPen]= useState(0);
-
+const BottomRow = props => {
   return (
-
-    <
-    div className = "bottomRow" >
-    <
-    div className = "down" >
-    <
-    h3 className = "down__title" > Penalties < /h3> <
-    div className = "down__value" > {
-      pen
-    } < /div> <
-    /div> <
-    div className = "toGo" >
-    <
-    h3 className = "toGo__title" > To Go < /h3> <
-    div className = "toGo__value" > 7 < /div> <
-    /div> <
-    div className = "ballOn" >
-    <
-    h3 className = "ballOn__title" > Ball on < /h3> <
-    div className = "ballOn__value" > 21 < /div> <
-    /div> <
-    div className = "quarter" >
-    <
-    h3 className = "quarter__title" > Half < /h3> <
-    div className = "quarter__value" > 4 < /div> <
-    /div> <
-    /div>
+    <div className="bottomRow">
+      <div className="down">
+        <h3 className="down__title"> Penalties </h3>
+        <div className="down__value">{props.penalties}</div>
+      </div>
+      <div className="ballOn">
+        <h3 className="ballOn__title"> Ball on </h3>
+        <div className="ballOn__value">{props.ballOn}</div>
+      </div>
+      <div className="quarter">
+        <h3 className="quarter__title"> Half </h3>
+        <div className="quarter__value">{props.half}</div>
+      </div>
+    </div>
   );
 };
 
